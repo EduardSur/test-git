2f2364b7fc69efa11a631f15f257df14e7269c1e
Eduard <shahaev_edik@mail.ru>
2024-07-22 11:11:28 +0800

    Add Homework

--- script.py
+++ script.py
@@ -8,20 +8,25 @@ def view():
         print(M[i][0],M[i][1],M[i][2],M[i][3])
     return None
 
-def Hod(n):
+def Hod():
     while True:
-        x = int(input('Введи Х'))
-        y = int(input('Введи У'))
-        if M[x][y] == '-':
-            break
-        print('Ячейка занята, введите другие координаты')
-    if n == 0:
-        M[x][y]='X'
-        n = 1
-    else:
+        str_x = input('Введи Х от 1 до 3')
+        str_y = input('Введи У от 1 до 3')
+        if str_x in ('1','2','3') and str_y in ('1','2','3'):
+            x=int(str_x)
+            y=int(str_y)
+            if M[x][y] == '-':
+                break
+            print('\nЯчейка занята, введите другие координаты\n')
+            view()
+        else:
+            print('\nНеправильные координаты! Попробуйте еще раз.\n')
+            view()
+    if n%2 == 0:
         M[x][y] = 'O'
-        n = 0
-    return n
+    else:
+        M[x][y] = 'X'
+    return None
 
 def Proverka():
     x = False
@@ -30,19 +35,23 @@ def Proverka():
         x = True
     if (M[1][1] == 'O' and M[1][2] == 'O' and M[1][3] == 'O') or (M[2][1] == 'O' and M[2][2] == 'O' and M[2][3] == 'O') or (M[3][1] == 'O' and M[3][2] == 'O' and M[3][3] == 'O') or (M[1][1] == 'O' and M[2][1] == 'O' and M[3][1] == 'O') or (M[1][2] == 'O' and M[2][2] == 'O' and M[3][2] == 'O') or (M[1][3] == 'O' and M[2][3] == 'O' and M[3][3] == 'O') or (M[1][1] == 'O' and M[2][2] == 'O' and M[3][3] == 'O') or (M[1][3] == 'O' and M[2][2] == 'O' and M[3][1] == 'O'):
         o = True
-    if x and o:
-        return print('\n-----Ничья-----')
-    elif x:
+    if x:
         return print('\n---Выиграл Х---')
-    elif o:
+    if o:
         return print('\n---Выиграл O---')
     return True
 
+view()
+print('\n-------------\n')
 while True:
-    n = Hod(n)
+    n +=1
+    Hod()
     print('\n-------------\n')
     view()
-    if n == 0:
-        if not Proverka():
-            break
+    win=Proverka()
+    if not win:
+        break
+    if n == 9:
+        print('\n-----Ничья-----\n')
+        break
     print('\n-------------\n')
